98539edc5dca0f06df56d601a211609531b5535c
Serhii Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-04-22 16:18:02 +0300

    added new page with infinity scroll

--- src/js/refs.js
+++ src/js/refs.js
@@ -3,5 +3,6 @@ const input = document.querySelector('.search-input');
 const gallery = document.querySelector('ul.gallery');
 const loader = document.querySelector('.loader');
 const loadMoreBtn = document.querySelector('.load-more');
+const guard = document.querySelector('.js-guard')
 
-export { form, input, gallery, loader, loadMoreBtn };
+export { form, input, gallery, loader, loadMoreBtn, guard };
